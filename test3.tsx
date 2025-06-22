3aee6d5ba1c93b42a196461c974a44099e74a9e1
cb21395 <cb21395@my.westlake.school.nz>
2025-06-23 09:00:07 +1200

    charactersprite added

---
+++ test3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="test3" tilewidth="16" tileheight="16" spacing="2" tilecount="25" columns="5">
+ <image source="../../2brickstiles.png" width="100" height="100"/>
+</tileset>
