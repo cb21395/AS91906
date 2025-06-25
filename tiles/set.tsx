0435d4a847427497a713a055a0cff6d148cea425
cb21395 <cb21395@my.westlake.school.nz>
2025-06-25 14:20:29 +1200

    tile making progress

---
+++ tiles/set.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="set" tilewidth="16" tileheight="16" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Y Brick.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="spike.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="basicbrick3.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="basicbrick2.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="basicbrick.png" width="16" height="16"/>
+ </tile>
+</tileset>
