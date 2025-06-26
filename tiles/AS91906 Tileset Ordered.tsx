cc7fec5e60f874e73a42ec4f1983921a02e8d19e
cb21395 <cb21395@my.westlake.school.nz>
2025-06-27 11:55:03 +1200

    test

--- tiles/AS91906 Tileset Ordered.tsx
+++ tiles/AS91906 Tileset Ordered.tsx
@@ -1,2 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="AS91906 Tileset Ordered" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.0" name="AS91906 Tileset Ordered" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="basicbrick.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="basicbrick2.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="basicbrick3.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="nosnowmountain.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="snowblock.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="spike.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="torch.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="Y Brick.png" width="16" height="16"/>
+ </tile>
+</tileset>
